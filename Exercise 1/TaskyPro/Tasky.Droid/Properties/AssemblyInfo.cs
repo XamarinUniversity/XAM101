f51a573e7207e66e37dfb21ab5894cf3ef92fb8d
Adam Patridge <patridge@gmail.com>
2018-01-03 10:09:26 -0700

    Update copyright notice year.

--- Exercise 1/TaskyPro/Tasky.Droid/Properties/AssemblyInfo.cs
+++ Exercise 1/TaskyPro/Tasky.Droid/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using Android.App;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("xamarin")]
+[assembly: AssemblyCopyright("Copyright (c) 2018 Xamarin Inc., Microsoft.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
